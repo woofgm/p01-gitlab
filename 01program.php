567bdc70a43bc45b583fb87d2c7f6aaa6c5cb848
woofgm <66486084+woofgm@users.noreply.github.com>
2020-06-05 18:45:16 +0200

    Update 01program.php

--- 01program.php
+++ 01program.php
@@ -4,6 +4,6 @@ echo ("<br> Jestem programem 01");
 echo ("<br>Test");
 echo ("<br>Test1");
 echo ("<br>Dalsza czesc programu");
-echu ("<br>Dalsza czesc programu222 z GitHuba");
+echo ("<br>Dalsza czesc programu222 z GitHuba");
 ?>
 
